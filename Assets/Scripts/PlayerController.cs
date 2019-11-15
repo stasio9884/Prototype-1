a88d7d44a297fc98beaf56f53161ef56851e1beb
courtney stasio <56166375+stasio9884@users.noreply.github.com>
2019-11-15 08:34:09 -0800

    made it sothe car turns insted of slides

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float forwardInput; 
-    public float horizontalInput; 
+    // private variables 
+    private float speed = 5.0f;
+    private float turnSpeed = 30.0f;
+    private float forwardInput; 
+    private float horizontalInput; 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // this is where we ge player input
         horizontalInput = Input.GetAxis("Horizontal"); 
         forwardInput = Input.GetAxis("Vertical");
-        // move the vehicle forward
-        transform.Translate(Vector3.forward* Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // moves the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // turns the vehicle 
+        transform.Rotate(Vector3.up,  Time.deltaTime * turnSpeed * horizontalInput );
     }
 }
